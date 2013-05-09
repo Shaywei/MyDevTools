fc432570b8b904aadb67396addd855c91dadc656
Shay Weiss <another.shay.weiss@gmail.com>
2013-05-09 21:31:49 +0300

    solved Yodle/triangle

---
+++ Python/Yodle/test_triangle.py
@@ -0,0 +1,62 @@
+import os
+from unittest import TestCase
+
+import triangle
+
+class TestTriangle(TestCase):
+    def test_triangle_example(self):
+        # Arrange
+        path_to_test_data = os.path.abspath(os.path.realpath('triangle_example.txt'))
+
+        expected = 27
+
+        # Act
+        result = triangle.max_path(path_to_test_data)
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_triangle_example2(self):
+        # Arrange
+        path_to_test_data = os.path.abspath(os.path.realpath('triangle_example2.txt'))
+
+        expected = 8 * 9 / 2
+
+        # Act
+        result = triangle.max_path(path_to_test_data)
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_triangle_example3(self):
+        # Arrange
+        path_to_test_data = os.path.abspath(os.path.realpath('triangle_example3.txt'))
+
+        expected = 8 * 9 / 2
+
+        # Act
+        result = triangle.max_path(path_to_test_data)
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_triangle_example4(self):
+        # Arrange
+        path_to_test_data = os.path.abspath(os.path.realpath('triangle_example4.txt'))
+
+        expected = 1
+
+        # Act
+        result = triangle.max_path(path_to_test_data)
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_triangle(self):
+        # Arrange
+        path_to_test_data = os.path.abspath(os.path.realpath('triangle.txt'))
+
+        expected = 1
+
+        # Act
+        print triangle.max_path(path_to_test_data)
---
+++ Python/Yodle/triangle.py
@@ -0,0 +1,35 @@
+__author__ = 'lost_dm'
+
+def max_path(path_to_file):
+    '''
+        solves http://www.yodlecareers.com/puzzles/triangle.html
+        Note that problem is undefined in case where there are adjecent identical numbers:
+        0
+        1 1
+        3 0 2
+
+        possible output can be either 4 or 3 depending on which 1 was chosen in second row.
+    '''
+
+    sum = index = 0
+    with open(path_to_file, 'r') as target_file:
+        for i, line in enumerate(target_file):
+            try:
+                line_as_list = [int(num_str) for num_str in line.strip().split()] # Read just 1 line at a time vs. loading all of the file to a list of lists
+            except Exception as e:
+                print "exception in line ", i, ":\n " + line
+                print e
+
+            assert len(line_as_list) == i + 1 # Making sure the triangle is valid
+
+            if i == 0:
+                sum = line_as_list[i] # Starting condition
+            else:
+                candidate1, candidate2 = line_as_list[index:index+2]
+                index, sum = (index, sum + candidate1) if candidate1 > candidate2 else (index + 1, sum + candidate2)
+        return sum
+
+
+
+
+
