3cf5208e971d9ff6cad3d03f8407a0d0a1bec8c1
Shay Weiss <another.shay.weiss@gmail.com>
2014-01-30 20:33:38 +0200

    implemented constuctions, insert, search __eq__ and __repr__ + tests

--- Python/BasicDataStructures/bst.py
+++ Python/BasicDataStructures/bst.py
@@ -1,35 +1,51 @@
 class BST(object):
-    __slots__ = ['item', '_next']
+    __slots__ = ['item', 'left', 'right']
 
-    def __init__(self, item, _next=None):
-        pass
+    def __init__(self, item, _left=None, _right=None):
+        self.item = item
+        self.left = _left
+        self.right = _right
 
     def insert(self, item):
-        pass
+        if item < self.item:
+            if self.left is None:
+                self.left = BST(item)
+            else:
+                self.left.insert(item)
+        else:
+            if self.right is None:
+                self.right = BST(item)
+            else:
+                self.right.insert(item)
 
     def search(self, item):
-        pass
+        if self.item == item:
+            return self
+
+        elif item < self.item:
+            if self.left is None:
+                return None
+            return self.left.search(item)
+        else:
+            if self.right is None:
+                return None
+            return self.right.search(item)
+
 
     def delete(self, item):
         pass
 
-    def __eq__(self, other):
+    def traverse(self):
         pass
+
+    def __eq__(self, other):
+        if self.item != other.item:
+            return False
+
+        return self.left == other.left and self.right == other.right
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
-
-    def __str__(self):
-        '''
-        s = ''
-        p = self
-        while p._next is not None:
-            s += str(p.item) + '->'
-            p = p._next
-        return s + str(p.item)
-        '''
-        pass
-        
     def __repr__(self):
-        return "BST(item=%r, _next=%r)" % (self.item ,self._next)
+        return "BST(item=%r, left=%r, right=%r)" % (self.item, self.left, self.right)
--- Python/BasicDataStructures/bst_tests.py
+++ Python/BasicDataStructures/bst_tests.py
@@ -3,58 +3,118 @@ import unittest
 from bst import BST
 
 def _factory(l):
-    pass
-    '''
-    _bst = bst(l[0])
+    _b = BST(l[0])
     for item in l[1:]:
-        _l.insert(item)
-    return _l
-    '''
+        _b.insert(item)
+    return _b
+
 
 
 class TestBST(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
+    
+    def _check_node(self, node, item, left_child, right_child):
+        self.assertEqual(item, node.item)
+        if left_child is None:
+            self.assertIsNone(node.left)
+        else:
+            self.assertEqual(left_child, node.left.item)
+
+        if right_child is None:
+            self.assertIsNone(node.right)
+        else:
+            self.assertEqual(right_child, node.right.item)
 
     def test_sanity(self):
         # Act
-        l = BST(1)
+        b = BST(1)
 
         # Assert
-        self.assertEqual(1, l.item)
-        self.assertEqual(None, l.next())
+        self.assertEqual(1, b.item)
+        self.assertEqual(None, b.left)
+        self.assertEqual(None, b.right)
 
-    def test_insert(self):
-        # Arrange
-        l = BST(1)
-
-        # Act
-        l.insert(2)
-        l.insert(3)
+    def test_insert_1(self):
+        # Arrange + Act
+        b = BST(1)
+        b.insert(2)
+        b.insert(3)
 
         # Assert
-        self.assertEqual(1, l.item)
-        self.assertEqual(2, l.next().item)
-        self.assertEqual(3, l.next().next().item)
-        self.assertEqual(None, l.next().next().next())
+        self._check_node(b, 1, None, 2)
+        self._check_node(b.right, 2, None, 3)
+        self._check_node(b.right.right, 3, None, None)
 
+    def test_insert_2(self):
+        # Arrange + Act
+        b = BST(1)
+        b.insert(3)
+        b.insert(2)
+
+        # Assert
+        self._check_node(b, 1, None, 3)
+        self._check_node(b.right, 3, 2, None)
+        self._check_node(b.right.left, 2, None, None)
+
+    def test_insert_3(self):
+        # Arrange + Act
+        b = BST(2)
+        b.insert(1)
+        b.insert(3)
+
+        # Assert
+        self._check_node(b, 2, 1, 3)
+        self._check_node(b.left, 1, None, None)
+        self._check_node(b.right, 3, None, None)
+    def test_insert_4(self):
+        # Arrange + Act
+        b = BST(2)
+        b.insert(3)
+        b.insert(1)
+
+        # Assert
+        self._check_node(b, 2, 1, 3)
+        self._check_node(b.left, 1, None, None)
+        self._check_node(b.right, 3, None, None)
+
+    def test_insert_5(self):
+        # Arrange + Act
+        b = BST(3)
+        b.insert(1)
+        b.insert(2)
+
+        # Assert
+        self._check_node(b, 3, 1, None)
+        self._check_node(b.left, 1, None, 2)
+        self._check_node(b.left.right, 2, None, None)
+
+    def test_insert_6(self):
+        # Arrange + Act
+        b = BST(3)
+        b.insert(2)
+        b.insert(1)
+
+        # Assert
+        self._check_node(b, 3, 2, None)
+        self._check_node(b.left, 2, 1, None)
+        self._check_node(b.left.left, 1, None, None)
+
+    '''
     def test_insert_illegal_type_raises_ValueError(self):
         # Arrange
-        l = BST(1)
+        b = BST(1)
 
         # Act + Assert
         with self.assertRaises(ValueError):
-            l.insert('2')
+            b.insert('2')
+    '''
 
     def test__eq__when_equal(self):
         # Arrange
-        l1 = _factory([1,2,3])
-        l2 = _factory([1,2,3])
+        b1 = _factory([2,3,1])
+        b2 = _factory([2,1,3])
 
         #Assert
-        self.assertTrue(l1 == l2)
+        self.assertEqual(b1, b2)
 
     def test__eq__when_not_equal(self):
         # Arrange
@@ -64,74 +124,62 @@ class TestBST(unittest.TestCase):
         #Assert
         self.assertFalse(l1 == l2)
 
+
     def test_search_contains_return_list(self):
         # Arrange
-        l = _factory([1,2,3,4,5])
-        expected = _factory([4,5])
+        b = _factory([1,3,2,4])
+        expected = _factory([3,4,2])
 
         # Act
-        actual = l.search(4)
+        actual = b.search(3)
 
         # Assert
         self.assertEqual(expected, actual)
 
     def test_search_not_contains_return_None(self):
         # Arrange
-        l = _factory([1,2,3,4,5])
+        b = _factory([1,2,3,4,5])
 
         # Act + Assert
-        self.assertIsNone(l.search(6))
-
-    def test_delete_list_item(self):
+        self.assertIsNone(b.search(6))
+    '''
+    def test_traverse(self):
         # Arrange
-        l = _factory([1,2,3,4,5])
+        b = _factory([1,2,3,4,5])
 
         # Act
-        BST.delete_list(l, 3)
+        b.delete_list(l, 3)
 
         # Assert
         self.assertEqual(_factory([1,2,4,5]), l)
 
-    def test_delete_list_item_that_doesnt_exist_raise_value_error(self):
-        # Arrange
-        l = _factory([1,2,3,4,5])
-
-        # Act + Assert
-        with self.assertRaises(ValueError):
-            BST.delete_list(l, 6)
 
     def test_delete_item(self):
         # Arrange
-        l = _factory([1,2,3,4,5])
+        b = _factory([1,2,3,4,5])
 
         # Act
-        l.delete_list(l, 3)
+        b.delete_list(l, 3)
 
         # Assert
         self.assertEqual(_factory([1,2,4,5]), l)
 
     def test_delete_item_that_doesnt_exist_raise_value_error(self):
         # Arrange
-        l = _factory([1,2,3,4,5])
+        b = _factory([1,2,3,4,5])
 
         # Act + Assert
         with self.assertRaises(ValueError):
-            l.delete_list(l, 6)
+            b.delete_list(l, 6)
 
-    def test__str__(self):
+    def test__repr__(self):
         # Arrange
-        l = _factory([1,2,3,4,5])
+        b = _factory([1,2,3])
 
         # Act + Assert
-        self.assertEqual('1->2->3->4->5', str(l))
+        self.assertEqual('BST(item=1, _next=BST(item=2, _next=BST(item=3, _next=None)))', b.__repr__())
 
-    def test__str__(self):
-        # Arrange
-        l = _factory([1,2,3])
-
-        # Act + Assert
-        self.assertEqual('BST(item=1, _next=BST(item=2, _next=BST(item=3, _next=None)))', l.__repr__())
-
+    '''
 
 if __name__ == '__main__':
     unittest.main()
