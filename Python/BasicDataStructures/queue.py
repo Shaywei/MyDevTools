c483d7230bc68974b8f0d12d11011930d5a23ae1
Shay Weiss <another.shay.weiss@gmail.com>
2014-03-20 13:29:58 +0200

    Implementing a queue using a single singly LinkedList where the head of the queue is the last element of the LL (and we keep a pointer to it for easy push()-ing) and the back of the queue is the head of the LL (So it's easy to pop() elements)

---
+++ Python/BasicDataStructures/queue.py
@@ -0,0 +1,49 @@
+from linked_lists import LinkedList
+class Queue(object):
+    @staticmethod
+    def from_list(l):
+        _q = Queue()
+        for item in l:
+            _q.insert(item)
+        return _q
+
+    def __init__(self, max_size=None):
+        self._ll = None
+        self._head_of_queue = None
+        self.max_size = max_size
+
+    def push(self, item):
+        if self._head_of_queue is None:
+            self._ll = LinkedList(item)
+            self._head_of_queue = self._ll
+        else:
+            self._head_of_queue.insert(item)
+            self._head_of_queue = self._head_of_queue.next_l
+
+    def to_list(self):
+        return self._ll.to_list() if self._ll is not None else []
+
+    def pop(self):
+        if len(self) == 0:
+            raise ValueError("Can't pop from an empty queue")
+
+        ans = self._ll.item
+        self._ll = self._ll.next_l
+        if self._ll is None:
+            self._head_of_queue = None
+        return ans
+
+    def __len__(self):
+        return 0 if self._ll is None else len(self._ll)
+
+    def __eq__(self, other):
+        return self._ll == other._ll
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __iter__(self):
+        return iter(self.to_list())
+
+    def __str__(self):
+        return '->'.join(map(str, self.to_list()[::-1]))
---
+++ Python/BasicDataStructures/queue_test.py
@@ -0,0 +1,97 @@
+import unittest
+
+from queue import Queue
+
+def _factory(l):
+    _q = Queue()
+    for item in l:
+        _q.push(item)
+    return _q
+
+class TestQueue(unittest.TestCase):
+    def test_push_pop_single_element(self):
+        # Arrange
+        q = Queue()
+
+        # Act
+        q.push('a')
+
+        # Assert
+        self.assertEqual(1, len(q))
+        self.assertEqual('a', q.pop())
+        self.assertEqual(0, len(q))
+        
+    def test_push_pop_three_element(self):
+        # Arrange
+        q = Queue()
+
+        # Act
+        q.push('a')
+        q.push('b')
+        q.push('c')
+
+        # Assert
+        self.assertEqual(3, len(q))
+        self.assertEqual('a', q.pop())
+        self.assertEqual(2, len(q))
+
+    def test_pop_on_empty_queue_raises_ValueError(self):
+        # Arrange
+        q = Queue()
+
+        # Act + Assert
+        with self.assertRaises(ValueError):
+            q.pop()
+
+    def test__eq__when_equal1(self):
+        # Arrange
+        q1 = _factory([1,2,3])
+        q2 = _factory([1,2,3])
+
+        #Assert
+        self.assertTrue(q1 == q2)
+
+    def test__eq__when_equal2(self):
+        # Arrange
+        q1 = Queue()
+        q2 = Queue()
+
+        #Assert
+        self.assertTrue(q1 == q2)
+
+    def test__eq__when_not_equal(self):
+        # Arrange
+        q1 = _factory([1,2,3])
+        q2 = _factory([1,3,2])
+
+        #Assert
+        self.assertFalse(q1 == q2)
+
+    def test_to_list(self):
+        q = _factory([1,2,3,4,5])
+        self.assertEqual([1,2,3,4,5], q.to_list())
+
+    def test_iteration(self):
+        # Arrange
+        q = _factory([1,2,3,4,5])
+
+        # Act + Assert                
+        for item, i in zip(q, range(1,6)):
+            self.assertEqual(item, i)
+
+    def test__len__(self):
+        # Arrange
+        q = _factory([1,2,3,4,5])
+
+        # Act + Assert
+        self.assertEqual(5, len(q))
+    def test__str__(self):
+        # Arrange
+        q = _factory([1,2,3,4,5])
+
+        # Act + Assert
+        self.assertEqual('5->4->3->2->1', str(q))
+
+if __name__ == '__main__':
+    unittest.main()
+
